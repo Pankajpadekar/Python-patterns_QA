d38013845ac74d52836e01344b0b1cc6d24f9113
Pankaj Padekar <padekarpankaj@gmail.com>
2024-02-27 00:19:01 +0530

    solved reversed  pyramid pattern

---
+++ 10_reversed_pyramid_pattern.py
@@ -0,0 +1,17 @@
+# The reverse pyramid pattern is the same as the pyramid pattern but it is upside down. See the pattern up there.
+
+# Just like the pyramid pattern, the reverse pyramid pattern follows the same logic. The only difference is that we have to print the spaces and stars with reverse logic.
+
+# *********
+#  *******
+#   *****
+#    ***
+#     *
+
+while True:
+
+    n = int(input("Enter the number : "))
+
+    for i in range (n+1):
+
+        print(" " * i + "*" * (n-i) + "*" * (n-(i-1)) + " " * i )
