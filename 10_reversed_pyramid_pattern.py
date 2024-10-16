f9d0e0aab6ec138b44a3ebebb96aea4737c5ccea
Pankaj Padekar <padekarpankaj@gmail.com>
2024-10-16 14:09:42 +0530

    made some corrections

--- 10_reversed_pyramid_pattern.py
+++ 10_reversed_pyramid_pattern.py
@@ -12,6 +12,6 @@ while True:
 
     n = int(input("Enter the number : "))
 
-    for i in range (n+1):
+    for i in range (1,n+1):
 
         print(" " * i + "*" * (n-i) + "*" * (n-(i-1)) + " " * i )
--- 11_Diamond_star_pattern.py
+++ 11_Diamond_star_pattern.py
@@ -18,14 +18,10 @@ while True:
 
     n = int(input("Enter the number : "))
 
-    for i in range(1, n+1):
+    for i in range(n+1):
 
-
-        print(" " * (n-i) + "*" * (2*i-1) )
-
+        print(" " * (n-i) + "*" * (2*i-1))
 
     for i in range(n+1):
 
-        print(" " * i + "*" * (n-i) + "*" * (n-(i-1)) + " " * i  )
-
-
+        print(" " * i + "*" * (n-i) + "*" * (n-(i-1)) + " " * i)
